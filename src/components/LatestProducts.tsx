d66669c3e29184a1b3a950e95849851a91e84637
Faraz Alam <153891083+faraz141@users.noreply.github.com>
2024-12-10 23:09:02 +0500

    set pixel perfect

--- src/components/LatestProducts.tsx
+++ src/components/LatestProducts.tsx
@@ -9,7 +9,7 @@ import { FaSearchPlus } from 'react-icons/fa';
 function LatestProducts() {
   const products = [
     { id: 1, img: '/images/product1.png' },
-    { id: 2, img: '/images/product2.png', sale: true },
+    { id: 2, img: '/images/product-2.png' },
     { id: 3, img: '/images/product3.png' },
     { id: 4, img: '/images/product4.png' },
     { id: 5, img: '/images/product5.png' },
@@ -43,48 +43,56 @@ function LatestProducts() {
         {/* Product Grid */}
         <div className="w-full md:w-[80%] lg:w-[1177px] max-w-screen-xl mx-auto place-items-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
           {products.map((product, index) => (
-            <div key={product.id} className="relative group">
+            <div
+              key={product.id}
+              className="w-[360px] h-[306px] bg-white  relative group"
+            >
               {/* Product Image */}
-              <div className="w-full bg-gray-200 flex justify-center items-center relative overflow-hidden h-[300px] transition-all duration-300 group-hover:bg-white">
+              <div className="w-[360px]  h-[270px] bg-[#f7f7f7] flex flex-col justify-center items-center relative overflow-hidden transition-all duration-300 group-hover:bg-white">
                 {/* Sale Tag */}
-                {product.sale && (
-                  <span className="absolute top-2 left-2 bg-[#3F509E] text-white text-sm px-3 py-1 rounded">
-                    Sale
-                  </span>
-                )}
+
+                <span className="opacity-0 group-hover:opacity-100 absolute top-4 left-4 bg-[#3F509E] text-white text-sm px-3 py-1 -rotate-[30deg] rounded">
+                  Sale
+                </span>
+                <div className="absolute bottom-6 flex items-center justify-center flex-col left-4 space-y-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <button className="bg-white p-2 rounded-full shadow group-hover:bg-[#eeeffb]">
+                    <LuShoppingCart className="w-6 h-6 text-[#3F509E]" />
+                  </button>
+                  <button className="bg-white p-2 ">
+                    <FaRegHeart className="w-4 h-4 text-[#3F509E]" />
+                  </button>
+                  <button className="bg-white p-2 ">
+                    <FaSearchPlus className="w-4 h-4 text-[#3F509E]" />
+                  </button>
+                </div>
                 <Image
                   src={product.img}
-                  width={200}
-                  height={200}
+                  width={235}
+                  height={235}
                   alt="Comfy Handy Craft"
-                  className="object-contain"
+                  className="object-contain "
                 />
 
                 {/* Icons */}
-                <div className="absolute top-2 right-2 space-y-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                  <button className="bg-white p-2 rounded-full shadow">
-                    <FaRegHeart className="w-6 h-6 text-[#3F509E]" />
-                  </button>
-                  <button className="bg-white p-2 rounded-full shadow">
-                    <FaSearchPlus className="w-6 h-6 text-[#3F509E]" />
-                  </button>
-                  <button className="bg-white p-2 rounded-full shadow">
-                    <LuShoppingCart className="w-6 h-6 text-[#3F509E]" />
-                  </button>
-                </div>
-              </div>
 
-              {/* Product Details */}
-              <div className="text-center mt-4">
-                <h3 className="text-lg font-semibold text-[#3F509E]">
-                  Comfy Handy Craft
-                </h3>
-                <div className="mt-2 text-gray-600 flex justify-center items-center gap-2">
-                  <span className="text-red-600 font-medium line-through">
-                    $65.00
-                  </span>
-                  <span className="text-gray-800">$42.00</span>
+                {/* Product Details */}
+              </div>
+              <div>
+                <div className="flex flex-row items-center justify-between">
+                  <h3 className="text-lg font-semibold text-[#3F509E]">
+                    Comfy Handy Craft
+                  </h3>
+                  {/* <div className="mt-2 text-gray-600 flex justify-center items-center gap-2"> */}
+                  <div>
+                    <span className="text-gray-800 mx-4">$42.00</span>
+                    <span className="text-red-600 font-medium line-through">
+                      $65.00
+                    </span>
+                  </div>
+
+                  {/* </div> */}
                 </div>
+                <div className="w-[153.73px] top-[305.04px] border-2 border-[#eeeffb] flex items-start justify-start rotate-[-0.36deg]"></div>
               </div>
             </div>
           ))}
